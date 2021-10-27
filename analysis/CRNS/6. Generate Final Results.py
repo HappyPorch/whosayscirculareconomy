1c3cf0ed54bee24ec79a16dc812fc285767315f4
barryokane <barry@happyporch.com>
2021-10-27 16:38:56 +0100

    Initial CRNS results display & filtering

--- analysis/CRNS/6. Generate Final Results.py
+++ analysis/CRNS/6. Generate Final Results.py
@@ -18,8 +18,12 @@ outputFile = os.path.join(sourceDir, "6. CE_Search_CRNS_{}.json".format(today.st
 with open(data.get_temp(sourceFile), 'r') as myfile:
     sourceData=myfile.read()
 
-results = json.loads(sourceData)
+sourceJson = json.loads(sourceData)
 
+results = []
+for row in sourceJson:
+    row["org"]["categories"] = [c["name"] for c in row["org"]["categories"]]
+    results.append(row)
 
 # read file
 with open(data.get_temp(allSearchedSourceFile), 'r') as myfile:
@@ -31,24 +35,35 @@ allIds = set(x["id"] for x in allSearchedSource)
 searchFoundIds = set(x["org"]["id"] for x in results)
 searchNotFoundIds = allIds.difference(searchFoundIds)
 unmatched = list(x for x in allSearchedSource if x["id"] in searchNotFoundIds)
-for c in unmatched:
+for row in unmatched:
+    row["categories"] = [c["name"] for c in row["categories"]]
     result = {
-        "org": c,
+        "org": row,
         "summary": {
             "search_term_found": 0
         }
     }
     results.append(result)
 
-
+# counts the original list of sites
 with open(originalSourceFile, 'r') as myfile:
     originalSourceFileData=myfile.read()
 originalSourceJson = json.loads(originalSourceFileData)
 
+#get distinct categories
+allCats = []
+for r in results:
+    cats = r["org"]["categories"]
+    allCats += cats
+distinctCats = list(set(allCats))
+distinctCats.sort()
+
+
 output = {
     "meta": {
     "search_date" : today.strftime("%d %b, %Y"),
-    "source_count": len(originalSourceJson)
+    "source_count": len(originalSourceJson),
+    "cats": distinctCats
     },
     "data": results
 }
--- web/crns/crns.js
+++ web/crns/crns.js
@@ -16,17 +16,30 @@ function setUp() {
 
         rawData = d;
         summaryDisplay(rawData);
+        for (let c in rawData.meta.cats) {
+            let cat = rawData.meta.cats[c];
+            $('#filterSummaryBy').append(`<option value="${cat}">where Category includes ${cat}</option>`);
+        }
+
+
+        $('.runSummaryFilter').change(function () {
+            summaryDisplay(rawData);
+        });
         
     });
 
 }
 
-function summaryDisplay(d) {
+function summaryDisplay(rawData) {
 
     $('#searchDateDisplay').html(rawData.meta.search_date)
     $('#originalCountDisplay').html(rawData.meta.source_count)
     
     var d = rawData.data;
+    var filter = $('#filterSummaryBy').val();
+    if (filter) {
+        d = filter_bycat(d, filter);
+    }
 
     $('#countDisplay_total').html(d.length)
     termFoundCount = filter_found(d).length;
@@ -34,9 +47,13 @@ function summaryDisplay(d) {
     $('#countDisplay_termfound_percent').html(asPercent(termFoundCount, d.length))
     $('#countDisplay_termnotfound').html(d.length-termFoundCount)
     $('#countDisplay_termnotfound_percent').html(asPercent(d.length-termFoundCount, d.length))
+
 }
 
 
+function filter_bycat(d, cat) {
+    return d.filter(function (x) {return x.org.categories.includes(cat)});
+}
 function filter_found(d, mincount) {
     if (mincount) {
         return d.filter(function (x) { return x.summary.search_term_found == 1 && x.summary.results_count >= mincount});
@@ -48,4 +65,9 @@ function filter_notfound(d) {
         return d.filter(function (x) { return x.summary.search_term_found == 0});
     }
     return d.filter(function (x) { return x.summary.search_term_found == 0});
+}
+
+function getCategories(d) {
+ var cats = d.data.map(item => item.age)
+        filter((value, index, self) => self.indexOf(value) === index)
 }
